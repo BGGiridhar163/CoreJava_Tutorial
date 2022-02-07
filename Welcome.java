15e1db980d46842a6e8f2f3dea367d4968a90626
BGGiridhar163 <34038496+BGGiridhar163@users.noreply.github.com>
2022-02-08 01:05:19 +0530

    FistProgram

---
+++ Welcome.java
@@ -0,0 +1,18 @@
+class Welcome{
+	String name;
+	String msg;
+	
+	public void storeDeatils(String n, String m){
+		name = n;
+		msg = m;
+	}
+	public void displayDetails(){
+		System.out.println("Hello "+name);
+		System.out.println(msg);
+	}
+	public static void main(String[] args){
+		Welcome w = new Welcome();
+		w.storeDeatils("Giridhar","Welcome to Java World..!");
+		w.displayDetails();
+	}
+}
